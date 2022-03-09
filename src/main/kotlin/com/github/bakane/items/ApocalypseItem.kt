f7a324f60af65a19b44dcc72e41ca2dea1d1fc5f
baka <dyatlovleonid@gmail.com>
2022-03-09 10:57:38 +0300

    added item rarity

--- src/main/kotlin/com/github/bakane/items/ApocalypseItem.kt
+++ src/main/kotlin/com/github/bakane/items/ApocalypseItem.kt
@@ -5,18 +5,36 @@ import com.github.jenya705.cmscore.module.item.CustomItem
 /**
  * An apocalypse item.
  *
+ * @param defaultRarity An item's default rarity.
  * @author bakane
  */
-interface ApocalypseItem : CustomItem {
+abstract class ApocalypseItem(defaultRarity: ItemRarity): CustomItem {
     /**
      * An item's rarity.
      */
-    var rarity: ItemRarity
+    var rarity: ItemRarity = defaultRarity
+        private set
 
     /**
      * Upgrades an item to another rarity.
      *
      * @param finalRarity A rarity an item is upgraded to.
      */
-    fun upgrade(finalRarity: ItemRarity)
+    open fun upgrade(finalRarity: ItemRarity? = null) {
+        if (finalRarity == null) {
+            rarity = when (rarity) {
+                ItemRarity.COMMON -> ItemRarity.UNCOMMON
+                ItemRarity.UNCOMMON -> ItemRarity.RARE
+                ItemRarity.RARE -> ItemRarity.EPIC
+                ItemRarity.EPIC -> ItemRarity.MYTHIC
+                ItemRarity.MYTHIC -> ItemRarity.LEGENDARY
+                ItemRarity.LEGENDARY -> throw Exception("Can't upgrade, rarity already at legendary")
+            }
+            return
+        }
+
+        if (finalRarity < rarity) throw IllegalArgumentException("Final rarity is more than current rarity")
+
+        rarity = finalRarity
+    }
 }
